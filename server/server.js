acbecea252839bea39c43beacebdd8e5fb493f04
Tosif Raza <razatosif206@gmail.com>
2026-09-26 01:44:48 +0530

    change port

--- server/server.js
+++ server/server.js
@@ -6,26 +6,16 @@ const { Server } = require("socket.io");
 const setupSocket = require("./socket");
 
 const app = express();
-
 const server = http.createServer(app);
 
 const io = new Server(server);
 
-
-// Serve frontend files
 app.use(express.static(path.join(__dirname, "..", "public")));
 
-
-// Setup Socket.IO
 setupSocket(io);
 
+const PORT = process.env.PORT || 10000;
 
-// Port
-const PORT = 3000;
-
-server.listen(PORT, () => {
-    console.log("--------------------------------");
-    console.log("Real-Time Chat Server Started");
-    console.log(`Open: http://localhost:${PORT}`);
-    console.log("--------------------------------");
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
